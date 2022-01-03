d944e153418ecb04ab7aa6b40a87afa5c15717ff
Danezis <kdanezis@gmail.com>
2022-01-03 22:44:54 +0100

    :lipstick: minor tweak for the task line

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -38,8 +38,8 @@
 </script>
 <defs>
     <g id="T{i}">
-        <text text-anchor="end" x="150" y="10.5" font-family="Verdana" font-size="9" fill="{leftLabel}">{$store.tasks[i].label}</text>
-        <line stroke-dasharray="0.5 2" x1="155" y1="8" x2="{xGrayPosition - 5}" y2="8" stroke="{dottedLine}" />
+        <text text-anchor="end" x="{Constantes.GRID.MIDDLE_X - 5}" y="10.5" font-family="Verdana" font-size="9" fill="{leftLabel}">{$store.tasks[i].label}</text>
+        <line stroke-dasharray="0.5 2" x1="{Constantes.GRID.MIDDLE_X}" y1="8" x2="{xGrayPosition - 5}" y2="8" stroke="{dottedLine}" />
         {#if $store.tasks[i].progress < 100}
         <rect x="{xGrayPosition}" y="0" width="{widthGray}" height="15" rx="5" ry="5" style="fill: {grey}; stroke: {greyStroke}; stroke-width: 0.05em;"/>    
         {/if}
