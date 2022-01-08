91e7c1db5d8014e550969c73cd688b97ff21add5
Danezis <kdanezis@gmail.com>
2022-01-08 14:07:53 +0100

    :lipstick: little improvement for milestones height

--- src/lib/Milestone.svelte
+++ src/lib/Milestone.svelte
@@ -5,6 +5,7 @@
 
     export let milestone: Struct.Milestone;
     export let i : number;
+    let level = i % 2
 
     let xMilestonePosition =  (milestone.date.getTime() - $store.start.getTime()) / ($store.end.getTime() - $store.start.getTime()) * Constantes.GRID.MIDDLE_WIDTH
 </script>
@@ -13,10 +14,14 @@
 <defs>
     <g id="M{i}">
         <use x="0" y="0" href="#map"/>
+        {#if level == 0}
         <line stroke-dasharray="1" x1="10" y1="20" x2="10" y2="50" stroke="#000" />
-        <text x="17" y="9" font-family="Verdana" font-size="10" fill="#000">{milestone.label}</text>
-        <text x="17" y="18" font-family="Verdana" font-size="8" fill="#44546A">{milestone.date.getDate()} {Constantes.MONTHS[milestone.date.getMonth()]}</text>
+        {:else}
+        <line stroke-dasharray="1" x1="10" y1="20" x2="10" y2="25" stroke="#000" />
+        {/if}
+        <text x="17" y="9" font-family="Verdana" font-size="10" fill="#000"  id="tata{i}">{milestone.label}</text>
+        <text x="17" y="18" font-family="Verdana" font-size="8" fill="#44546A">{milestone.date.getDate()}-{Constantes.MONTHS[milestone.date.getMonth()]}</text>
     </g>
         
 </defs>
-<use x="{xMilestonePosition - 10}" y="0" href="#M{i}"/>
+<use x="{xMilestonePosition - 10}" y="{level * 25}" href="#M{i}" id="toto{i}"/>
--- src/lib/Milestones.svelte
+++ src/lib/Milestones.svelte
@@ -3,13 +3,19 @@
     import { Constantes } from './constantes.class';
     import Milestone from './Milestone.svelte';
     import { store } from './stores';
+    import type { Struct } from './struct.class';
 
+    function compareMilestone(a : Struct.Milestone, b : Struct.Milestone){
+        if(a.date > b.date){return 1}
+        if(a.date < b.date){return -1}
+        return 0
+    }
 </script>
 
     
 <defs>
     <g id="milestones">
-        {#each $store.milestones as milestone, i}
+        {#each $store.milestones.sort(compareMilestone) as milestone, i}
             {#if milestone.isShow}
                 <Milestone i={i} milestone={milestone}/>
             {/if}
