adefdf3aae53e145a6ffad7e94584632fa64c697
Danezis <kdanezis@gmail.com>
2022-01-10 21:32:31 +0100

    :sparkles: add drag&drop support for milestones date

--- src/lib/Milestone.svelte
+++ src/lib/Milestone.svelte
@@ -3,25 +3,25 @@
     import { Constantes } from './constantes.class';
     import { store } from "./stores";
 
-    export let milestone: Struct.Milestone;
-    export let i : number;
+    export let milestone: Struct.Milestone
+    export let i : number
+    export let down
     let level = i % 2
 
     let xMilestonePosition =  (milestone.date.getTime() - $store.start.getTime()) / ($store.end.getTime() - $store.start.getTime()) * Constantes.GRID.MIDDLE_WIDTH
 </script>
 
-    
 <defs>
-    <g id="M{i}">
+    <g id="M{milestone.id}">
         <use x="0" y="0" href="#map"/>
         {#if level == 0}
         <line stroke-dasharray="1" x1="10" y1="20" x2="10" y2="50" stroke="#000" />
         {:else}
         <line stroke-dasharray="1" x1="10" y1="20" x2="10" y2="25" stroke="#000" />
         {/if}
-        <text x="17" y="9" font-family="Verdana" font-size="10" fill="#000"  id="tata{i}">{milestone.label}</text>
+        <text x="17" y="9" font-family="Verdana" font-size="10" fill="#000">{milestone.label}</text>
         <text x="17" y="18" font-family="Verdana" font-size="8" fill="#44546A">{milestone.date.getDate()}-{Constantes.MONTHS[milestone.date.getMonth()]}</text>
     </g>
         
 </defs>
-<use x="{xMilestonePosition - 10}" y="{level * 25}" href="#M{i}" id="toto{i}"/>
+<use x="{Constantes.GRID.MIDDLE_X + xMilestonePosition - 10}" y="{level * 25}" href="#M{milestone.id}" class="milestoneSection" on:mousedown={down}  />
--- src/lib/Milestones.svelte
+++ src/lib/Milestones.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { browser } from '$app/env';
+
     
     import { Constantes } from './constantes.class';
     import Milestone from './Milestone.svelte';
@@ -19,18 +21,152 @@
     });
     //Sort by date ASC
     milestones = milestones.sort(compareMilestone)
+
+
+    let svgUseNode: HTMLElement  = null
+    let svgGNode: HTMLElement  = null
+    let svgDefsNode: HTMLElement  = null
+    let target: HTMLElement = null
+    let hoverGroup = false
+    const GHOST_USE_NODE_ID = "ghostUseNode"
+    const GHOST_G_NODE_ID = "ghostGNode"
+    const GHOST_DEFS_NODE_ID = "ghostDefsNode"
+    /**
+     * Triggered every time user try to "grab" an svg group of Milestone
+     * @param event the event mousedown
+     */
+    function down(event){
+        target = event.target
+        svgUseNode = event.target.cloneNode(true)
+        svgGNode = <HTMLElement> document.getElementById(svgUseNode.getAttribute("href").substring(1)).cloneNode(true) // substring : remove prefix '#'
+    
+        //Find the last node of our SVG group
+        let endMilestoneNode = document.getElementById("endMilestoneNode")
+
+        //create ghost node <defs>
+        svgDefsNode = document.createElement("defs")
+        svgDefsNode.setAttribute("id", GHOST_DEFS_NODE_ID)
+        endMilestoneNode.parentNode.insertBefore(svgDefsNode, endMilestoneNode)
+
+        //create ghost node <g>
+        svgGNode.id = GHOST_G_NODE_ID
+        svgDefsNode.appendChild(svgGNode)
+
+        //create ghost node <use>
+        svgUseNode.id = GHOST_USE_NODE_ID
+        svgUseNode.setAttribute("href", "#" + GHOST_G_NODE_ID)
+        endMilestoneNode.parentNode.insertBefore(svgUseNode, endMilestoneNode)
+    }
+
+    /**
+     * Triggered every time user release the left clic of the mouse
+     * @param event the event mouseup
+     */
+    function up(event){
+        if(svgUseNode && hoverGroup){
+            let newX = event.clientX / window.innerWidth * Constantes.GRID.ALL_WIDTH
+            let date = processNewDate(newX - Constantes.GRID.MIDDLE_X)
+            let idMilestone = target.getAttribute("href").substring(2) // #M999 => 999
+
+            let milestones = $store.getMilestonesById(parseInt(idMilestone))
+            milestones.date = date
+            $store.processLimites()
+            $store.milestones = $store.milestones
+            
+        }
+
+        //Reset vars
+        let ghostUseNode = document.getElementById(GHOST_USE_NODE_ID)
+        if(ghostUseNode) {
+            ghostUseNode.remove()
+        }
+        let ghostGNode = document.getElementById(GHOST_G_NODE_ID)
+        if(ghostGNode) {
+            ghostGNode.remove()
+        }
+        let svgDefsNode = document.getElementById(GHOST_DEFS_NODE_ID)
+        if(svgDefsNode) {
+            svgDefsNode.remove()
+        }
+        svgUseNode = null
+        svgGNode = null
+        svgDefsNode = null
+        target = null
+    }
+    
+    let recBox: DOMRect = null
+
+    /**
+     * Triggered every time user move the mouse
+     * @param event the event mousemove
+     */
+     function move(event){
+
+        if(!recBox && browser){
+            recBox = document.getElementById("milestonesSection").getBoundingClientRect()
+        }
+
+        if(hoverGroup && (event.clientX <= recBox.left || event.clientX >= recBox.right
+                || event.clientY <= recBox.top || event.clientY >= recBox.bottom)){
+            hoverGroup = false
+        }
+
+        if(!hoverGroup && event.clientX > recBox.left && event.clientX < recBox.right && event.clientY > recBox.top && event.clientY < recBox.bottom){
+            hoverGroup = true
+        }
+
+        if(hoverGroup){
+            //Moving ghostUseNode on the axe <===> 
+            if(svgUseNode){
+                let newX = event.clientX / window.innerWidth * Constantes.GRID.ALL_WIDTH
+                svgUseNode.setAttribute('x', `${newX - 10}`)
+
+                //Get new Date
+                let newDate: Date = processNewDate(newX - Constantes.GRID.MIDDLE_X)
+                let newDateLabel = newDate.getDate() + "-" + Constantes.MONTHS[newDate.getMonth()]
+                let svgGDateLabelNode : HTMLElement = <HTMLElement> svgGNode.lastChild
+                if(svgGDateLabelNode){
+                    svgGDateLabelNode.innerHTML = newDateLabel
+                }
+            } 
+        }
+    }
+
+    function processNewDate(newX: number){
+        let ratio = $store.start.getTime() + (newX / Constantes.GRID.MIDDLE_WIDTH * ($store.end.getTime() - $store.start.getTime()))
+        return new Date(ratio)
+    }
+
+
 </script>
 
-    
-<defs>
-    <g id="milestones">
-        {#each milestones as milestone, i}
-            {#if milestone.isShow}
-                <Milestone i={i} milestone={milestone}/>
-            {/if}
-        {/each}
-    </g>
-</defs>
+<svelte:window on:mouseup={up} on:mousemove="{move}"/>
+<rect id="milestonesSection" x="{Constantes.GRID.MIDDLE_X}" y="0" width="{Constantes.GRID.MIDDLE_WIDTH}" height="50" 
+    stroke-dasharray="0.5 2" style="" class:onhover={svgUseNode && hoverGroup} />
+{#each milestones as milestone, i}
+    {#if milestone.isShow}
+        <Milestone i={i} milestone={milestone} down={down}/>
+        <line id='endMilestoneNode' x1="0" y1="0" x2="0" y2="0" stroke="transparent" />
+    {/if}
+{/each}
 
-<use x="{Constantes.GRID.MIDDLE_X}" y="0" href="#milestones"/>
 
+<style>
+
+#milestonesSection {
+    stroke-width: 0.05em;
+    fill: transparent;
+}
+:global(#milestonesSection.onhover){
+    stroke: cadetblue;
+    cursor: grab;
+    fill: rgb(164, 185, 185)
+
+}
+:global(.milestoneSection){
+    cursor: grab;
+}
+:global(#ghostGNode){
+    opacity: 0.5;
+}
+</style>
