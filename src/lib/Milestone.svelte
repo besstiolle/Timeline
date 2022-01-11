391344e48c18e7a7eb9e56745966b63cf1c593ba
Danezis <kdanezis@gmail.com>
2022-01-11 16:27:55 +0100

    :recycle: reduce svg complexity

--- src/lib/Milestone.svelte
+++ src/lib/Milestone.svelte
@@ -13,7 +13,8 @@
 </script>
 
 <svg viewBox="0 0 {Constantes.GRID.ALL_WIDTH} {Constantes.GRID.MILESTONE_H + Constantes.GRID.BANNER_H + Constantes.GRID.ONE_TASK_H * Helpers.countVisibleTask($store.tasks) + Constantes.GRID.TODAY_H}" 
-     xmlns="http://www.w3.org/2000/svg" class="milestoneSection" on:mousedown={down} id="M{milestone.id}" x="{Constantes.GRID.MIDDLE_X + xMilestonePosition - 10}" y="{level * 25}" >
+     xmlns="http://www.w3.org/2000/svg" x="{Constantes.GRID.MIDDLE_X + xMilestonePosition - 10}" y="{level * 25}" 
+     class="milestoneSection" on:mousedown={down} id="M{milestone.id}" >
     <use x="0" y="0" href="#map"/>
     {#if level == 0}
     <line stroke-dasharray="1" x1="10" y1="20" x2="10" y2="50" stroke="#000" />
--- src/lib/Today.svelte
+++ src/lib/Today.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 
     import { Constantes } from './constantes.class';
+import { Helpers } from './helpers.class';
     import { store } from './stores';
 
     let toDisplay = ($store.start <= new Date() && $store.end >= new Date())
@@ -13,14 +14,12 @@
 </script>
 
 {#if toDisplay}
-    <defs>
-        <g id="today">
+<svg viewBox="0 0 {Constantes.GRID.ALL_WIDTH} {Constantes.GRID.MILESTONE_H + Constantes.GRID.BANNER_H + Constantes.GRID.ONE_TASK_H * Helpers.countVisibleTask($store.tasks) + Constantes.GRID.TODAY_H}" 
+     xmlns="http://www.w3.org/2000/svg" x="{Constantes.GRID.MIDDLE_X}" y="{Constantes.GRID.MILESTONE_H}"  >
+
             <rect x="0" y="20" width="{widthToday}" height="5" style="fill: {todayColor}"/>
             <polygon points='{widthToday},25 {widthToday + 5},40 {widthToday - 5},40' style='fill: {todayColor}' />
             <text x="{widthToday - 10}" y="50" font-family="Verdana" font-size="7" fill="{todayLabelColor}">Today</text>
             <line stroke-dasharray="0.5 2" x1="{widthToday}" y1="40" x2="{widthToday}" y2="100%" stroke="{todayColor}" />
-        </g>
-    </defs>
-
-    <use x="{Constantes.GRID.MIDDLE_X}" y="50" href="#today"/>
+</svg>
 {/if}
