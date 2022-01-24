33d9e6aa29b5702277381f670e95a87f2a8fd936
Danezis <kdanezis@gmail.com>
2022-01-24 21:51:18 +0100

    :bug: try to fix CI in netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		}),
 		target: '#svelte'
 	}
 };
