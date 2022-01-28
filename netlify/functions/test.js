73e05e0c9fbe7ee73f80cabe9cd625df73b67e66
Danezis <kdanezis@gmail.com>
2022-01-28 21:24:46 +0100

    :construction: test on netlify functions

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,10 @@
 exports.handler = async function () {
+    let secret = process.env.FOO_SECRET
+    if(!secret){
+      secret = "secret wasn't found in process.env.FOO_SECRET"
+    }
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Hello World' }),
+      body: JSON.stringify({ message: 'FOO_SECRET was ' + secret }),
     };
   };
