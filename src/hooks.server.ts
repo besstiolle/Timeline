a7b8ba9e127a0e0aba8f8ac1fb4bf06c293d6f62
Kevin Danezis <dev@kdanezis.fr>
2025-05-03 07:58:45 +0200

    moving sqlite database into ./db directory

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,19 @@
 import { initDatabase } from '$lib/database/initdatabase';
 import type { Handle } from '@sveltejs/kit';
 import Database from 'better-sqlite3';
+import fs from 'fs';
 
+const directory = 'db'
 export const handle: Handle = async ({ event, resolve }) => {
   if (!event.locals.db) {
+
+    //Create directory
+    if( !fs.existsSync(directory)){
+      fs.mkdirSync(directory) 
+    }
+
     // This will create the database within the `db.sqlite` file.
-    const db = new Database('db.sqlite')
+    const db = new Database(`${directory}/db.sqlite`)
     
     db.pragma('journal_mode = WAL');
 
